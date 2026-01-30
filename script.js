1677bb9696c71bf54e51e4c4ac3f59f7f7e59307
KashmirValleyNaturals <moinraja7889@gmail.com>
2026-01-30 20:36:36 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Smooth click feedback
-document.querySelectorAll('a').forEach(link => {
-  link.addEventListener('click', () => {
-    document.body.style.opacity = '0.97';
-    setTimeout(() => {
-      document.body.style.opacity = '1';
-    }, 150);
-  });
-});
